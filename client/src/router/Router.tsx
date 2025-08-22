320fa4022bd39087220347178886e2ae3d28b150
OfficialGopi <official.gopi@outlook.com>
2025-08-22 12:10:08 +0530

    Remove unnecessary file

--- client/src/router/Router.tsx
+++ client/src/router/Router.tsx
@@ -6,7 +6,6 @@ const MainLayout = lazy(() => import("@/components/layout/MainLayout"));
 
 //PAGES
 const Home = lazy(() => import("@/app/HomePage"));
-const CheckAuthStatus = lazy(() => import("@/app/CheckAuthStatus"));
 const Login = lazy(() => import("@/app/(auth)/Login"));
 
 const Router = () => {
@@ -14,7 +13,6 @@ const Router = () => {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/check-auth-status" element={<CheckAuthStatus />} />
       <Route element={<MainLayout />}>
         <Route path="/chats" element={<></>} />
         <Route path="/roadmaps" element={<></>} />
